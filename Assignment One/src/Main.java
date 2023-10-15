e370cae29b7a2ddd040b7a7af67b7c2198403b6f
theoliverlear <sigwartholiver2@gmail.com>
2023-10-15 14:53:10 -0500

    Add instance variables

--- Assignment One/src/Main.java
+++ Assignment One/src/Main.java
@@ -11,7 +11,28 @@
 // Last Modified: 10/12/2023                                                 |
 //----------------------------------------------------------------------------
 public class Main {
+ /*
+     Name	 Color	 Task	         Age
+     Jimmy	 Cyan	 Electrical      34
+     Steve	 Red	 Medbay	         21
+     Bart	 Lime	 Navigation      25
+     Mike	 Purple	 Communications  45
+     Jeb	 Yellow	 O2	             27
+     Dingus	 Green	 Admin	         64
+  */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Crewmember jimmy = new Crewmember("Jimmy", "Cyan",
+                                     "Electrical", 34, true);
+        Crewmember steve = new Crewmember("Steve", "Red",
+                                     "Medbay", 21, true);
+        Crewmember bart = new Crewmember("Bart", "Lime",
+                                    "Navigation", 25, true);
+        Crewmember mike = new Crewmember("Mike", "Purple",
+                                    "Communications", 45, true);
+        Crewmember jeb = new Crewmember("Jeb", "Yellow",
+                                   "O2", 27, true);
+        Crewmember dingus = new Crewmember("Dingus", "Green",
+                                      "Admin", 64, true);
+
     }
 }
