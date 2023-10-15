ad96b9eb9e9c4b29f3af6998216cb26dd39b9240
theoliverlear <sigwartholiver2@gmail.com>
2023-10-15 14:53:55 -0500

    Add alive data field

--- Assignment One/src/Crewmember.java
+++ Assignment One/src/Crewmember.java
@@ -1,23 +1,26 @@
+import java.util.ArrayList;
+
 public class Crewmember {
     private String name;
     private String color;
     private String task;
     private int age;
+    private boolean isAlive;
     private static int numCrewmembers = 0;
+    private static ArrayList<Crewmember> crewmembers = new ArrayList<>();
     //----------------------------Constructors--------------------------------
     public Crewmember() {
-        this.name = "John";
-        this.color = "red";
-        this.task = "carpenter";
-        this.age = 10;
-        numCrewmembers++;
+        this("John", "red", "carpenter", 10, true);
     }
-    public Crewmember(String name, String color, String task, int age) {
+    public Crewmember(String name, String color, String task, int age,
+                      boolean isAlive) {
         this.name = name;
         this.color = color;
         this.task = task;
         this.age = age;
+        this.isAlive = isAlive;
         numCrewmembers++;
+        crewmembers.add(this);
     }
     //------------------------------Getters-----------------------------------
     public String getName() {
@@ -32,9 +35,15 @@ public class Crewmember {
     public int getAge() {
         return this.age;
     }
+    public boolean getIsAlive() {
+        return this.isAlive;
+    }
     public static int getNumCrewmembers() {
         return numCrewmembers;
     }
+    public static ArrayList<Crewmember> getCrewmembers() {
+        return crewmembers;
+}
     //------------------------------Setters-----------------------------------
     public void setName(String name) {
         this.name = name;
@@ -47,6 +56,9 @@ public class Crewmember {
     }
     public void setAge(int age) {
         this.age = age;
+    }
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
     }
     //------------------------------Methods-----------------------------------
     public void details() {
