2546c2a9239f0f48806d1e72923ca3d21052884c
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2023-10-16 10:52:17 -0500

    Add comments
    Took 23 seconds

--- Assignment One/src/Imposter.java
+++ Assignment One/src/Imposter.java
@@ -1,13 +1,28 @@
+// Name: Oliver Sigwarth                                                     |
+// Assignment 1 - Among Us                                                   |
+// Program: Main.java                                                        |
+// Description:                                                              |
+/*----------------------------------------------------------------------------
+ * The goal of this assignment is to practice creating classes and objects.  |
+ * You will create a "Crewmember" class and an instance for each Crewmember  |
+ * in Among Us and implement methods to interact with those objects.         |
+----------------------------------------------------------------------------*/
+// Created: 10/12/2023                                                       |
+// Last Modified: 10/16/2023                                                 |
+//----------------------------------------------------------------------------
 public class Imposter extends Crewmember {
     public Imposter() {
         super();
     }
+    //----------------------------Constructors--------------------------------
     public Imposter(String name, String color, String task, int age,
                     boolean isAlive) {
         super(name, color, task, age, isAlive);
     }
+    //------------------------------Methods-----------------------------------
     public void kill(Crewmember crewmember) {
         if (crewmember.getIsAlive() && this.getIsAlive()) {
+            System.out.println(crewmember.getName() + " has been killed!");
             crewmember.setIsAlive(false);
         }
     }
