22619ec62a23816a6f1701b9fbc12dad57c72beb
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2023-10-16 10:51:54 -0500

    Add comments and color method
    Took 53 minutes

--- Assignment One/src/Crewmember.java
+++ Assignment One/src/Crewmember.java
@@ -1,3 +1,16 @@
+// Name: Oliver Sigwarth                                                     |
+// Assignment 1 - Among Us                                                   |
+// Program: Main.java                                                        |
+// Description:                                                              |
+/*----------------------------------------------------------------------------
+ * The goal of this assignment is to practice creating classes and objects.  |
+ * You will create a "Crewmember" class and an instance for each Crewmember  |
+ * in Among Us and implement methods to interact with those objects.         |
+----------------------------------------------------------------------------*/
+// Created: 10/12/2023                                                       |
+// Last Modified: 10/16/2023                                                 |
+//----------------------------------------------------------------------------
+
 import java.util.ArrayList;
 
 public class Crewmember {
@@ -7,7 +20,7 @@ public class Crewmember {
     private int age;
     private boolean isAlive;
     private static int numCrewmembers = 0;
-    private static ArrayList<Crewmember> crewmembers = new ArrayList<>();
+    private static ArrayList<Crewmember> crewmembersList = new ArrayList<>();
     //----------------------------Constructors--------------------------------
     public Crewmember() {
         this("John", "red", "carpenter", 10, true);
@@ -20,29 +33,35 @@ public class Crewmember {
         this.age = age;
         this.isAlive = isAlive;
         numCrewmembers++;
-        crewmembers.add(this);
+        crewmembersList.add(this);
     }
+    // For the following class, I am allowing access to all fields through
+    // getters. Setters are only allowed for the fields that will change
+    // during the gameplay. The two setters that are allowed are for the
+    // task and isAlive variables.
     //------------------------------Getters-----------------------------------
+    // For the sake of modularity, I'm including getters ands setters for all
+    // of the fields, even though I don't use them in this program.
     public String getName() {
         return this.name;
-    }
+    } // Get
     public String getColor() {
         return this.color;
-    }
+    } // Get
     public String getTask() {
         return this.task;
-    }
+    } // Get & Set
     public int getAge() {
         return this.age;
-    }
+    } // Get
     public boolean getIsAlive() {
         return this.isAlive;
-    }
+    } // Get & Set
     public static int getNumCrewmembers() {
         return numCrewmembers;
     }
     public static ArrayList<Crewmember> getCrewmembers() {
-        return crewmembers;
+        return crewmembersList;
 }
     //------------------------------Setters-----------------------------------
     public void setName(String name) {
@@ -62,7 +81,27 @@ public class Crewmember {
     }
     //------------------------------Methods-----------------------------------
     public void details() {
-        System.out.printf("Name: %s\nColor: %s\nTask: %s\nAge: %d\n",
-                          this.name, this.color, this.task, this.age);
+        System.out.printf("My name is %s, my color is %s, my task is %s," +
+                          " and I am %d years old.\n",
+                          this.name, this.withColor(this.color), this.task,
+                          this.age);
+    }
+    //
+    public String withColor(String phrase) {
+        String colorLowered = this.color.toLowerCase();
+        final String resetCode = "\u001B[0m";
+        String colorCode = switch (this.color.toLowerCase()) {
+            case "cyan" -> "\u001B[36m";
+            case "red" -> "\u001B[31m";
+            case "lime" -> "\u001B[32m";
+            case "purple" -> "\u001B[35m";
+            case "yellow" -> "\u001B[33m";
+            case "green" -> "\u001B[32m";
+            case "gray" -> "\u001B[37m";
+            default -> throw new IllegalStateException("Unexpected value: " +
+                                                        colorLowered);
+        };
+        String coloredPhrase = colorCode + phrase + resetCode;
+        return coloredPhrase;
     }
 }
