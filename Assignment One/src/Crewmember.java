9f55c164644a8e626bb9b4b6f716060f6c822de8
theoliverlear <sigwartholiver2@gmail.com>
2023-10-18 17:58:28 -0500

    Add comments and rename method

--- Assignment One/src/Crewmember.java
+++ Assignment One/src/Crewmember.java
@@ -8,7 +8,7 @@
  * in Among Us and implement methods to interact with those objects.         |
 ----------------------------------------------------------------------------*/
 // Created: 10/12/2023                                                       |
-// Last Modified: 10/16/2023                                                 |
+// Last Modified: 10/18/2023                                                 |
 //----------------------------------------------------------------------------
 import java.util.ArrayList;
 public class Crewmember {
@@ -17,7 +17,9 @@ public class Crewmember {
     private String task;
     private int age;
     private boolean isAlive;
-    private static int numCrewmembers = 0;
+    // The numCrewmembers variable is set to protected so the Imposter class
+    // can mutate the number when a crewmember is killed.
+    protected static int numCrewmembers = 0;
     private static ArrayList<Crewmember> crewmembersList = new ArrayList<>();
     //----------------------------Constructors--------------------------------
     public Crewmember() {
@@ -29,8 +31,11 @@ public class Crewmember {
     public Crewmember(String name, String color, String task, int age,
                       boolean isAlive) {
         // This constructor is used to create a crewmember with custom
-        // it is also used to keep track of the number of crewmembers and when
-        // called, it adds
+        // it is also used to keep track of the number of crewmembers by
+        // incrementing numCrewmembers when a new crewmember is instantiated.
+        // This constructor adds the crewmember to the crewmembersList to keep
+        // track of them so that the printRemainingCrewmembers() method can
+        // iterate through the list and print the remaining crewmembers.
         this.name = name;
         this.color = color;
         this.task = task;
@@ -39,14 +44,11 @@ public class Crewmember {
         numCrewmembers++;
         crewmembersList.add(this);
     }
-    // For the following class, I am allowing access to all fields through
-    // getters. Setters are only allowed for the fields that will change
-    // during the gameplay. The two setters that are allowed are for the
-    // task and isAlive variables.
     //------------------------------Getters-----------------------------------
     // Getters are allowed for all fields because they are all private and
     // cannot be accessed outside of the class. Per the rubric, all fields
-    // need to be accessed to demonstrate their functionality, pe
+    // need to be accessed to demonstrate their functionality and so they are
+    // safely accessible through getters.
     public String getName() {
         return this.name;
     } // Get
@@ -72,7 +74,9 @@ public class Crewmember {
     // Setters are only allowed for the fields that may change during the
     // program. The task may change during the program like in the game. The
     // isAlive variable may change during the program if the crewmember is
-    // killed by the imposter.
+    // killed by the imposter. The absence of these methods are meant to
+    // demonstrate that the fields should not be changed and is only allowed
+    // through the constructor.
     public void setTask(String task) {
         this.task = task;
     }
@@ -81,6 +85,9 @@ public class Crewmember {
     }
     //------------------------------Methods-----------------------------------
     public void details() {
+        // Prints the details of each crewmember. The details include the
+        // name, color, task, and age. The name and color are printed in their
+        // respective colors.
         System.out.printf("My name is %s, my color is %s, my task is %s," +
                           " and I am %d years old.\n",
                           this.withColor(this.name),
@@ -88,6 +95,9 @@ public class Crewmember {
     }
     //
     public String withColor(String phrase) {
+        // This method takes a phrase and colors it based on the color of the
+        // crewmember. The colors are based on the ANSI escape codes and they
+        // are assigned based on a switch of the color of the crewmember.
         String colorLowered = this.color.toLowerCase();
         final String resetCode = "\u001B[0m";
         String colorCode = switch (colorLowered) {
@@ -104,7 +114,10 @@ public class Crewmember {
         String coloredPhrase = colorCode + phrase + resetCode;
         return coloredPhrase;
     }
-    public static void remainingCrewmembers() {
+    public static void printRemainingCrewmembers() {
+        // This method prints the remaining crewmembers. It iterates through
+        // the crewmembersList and it will only print if the crewmember
+        // isAlive.
         System.out.println("The remaining crewmembers are:");
         for (Crewmember crewmember : crewmembersList) {
             if (crewmember.getIsAlive()) {
